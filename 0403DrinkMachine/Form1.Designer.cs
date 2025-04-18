27c969937c320efa16a9d57b9ff5c6dd1dabe75e
WaltDev29 <dwdr29@gmail.com>
2025-04-18 09:01:04 +0900

    first commit

---
+++ 0403DrinkMachine/Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace _0403DrinkMachine
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSelect1 = new System.Windows.Forms.Button();
+            this.btnSelect2 = new System.Windows.Forms.Button();
+            this.btnSelect3 = new System.Windows.Forms.Button();
+            this.btnSelect4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCharge = new System.Windows.Forms.Label();
+            this.drinkBox = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblMoneyLeft1 = new System.Windows.Forms.Label();
+            this.lblMoneyLeft2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.drinkBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSelect1
+            // 
+            this.btnSelect1.Location = new System.Drawing.Point(76, 231);
+            this.btnSelect1.Name = "btnSelect1";
+            this.btnSelect1.Size = new System.Drawing.Size(78, 36);
+            this.btnSelect1.TabIndex = 0;
+            this.btnSelect1.Text = "1";
+            this.btnSelect1.UseVisualStyleBackColor = true;
+            this.btnSelect1.Click += new System.EventHandler(this.btnSelect1_Click);
+            // 
+            // btnSelect2
+            // 
+            this.btnSelect2.Location = new System.Drawing.Point(254, 231);
+            this.btnSelect2.Name = "btnSelect2";
+            this.btnSelect2.Size = new System.Drawing.Size(78, 36);
+            this.btnSelect2.TabIndex = 1;
+            this.btnSelect2.Text = "2";
+            this.btnSelect2.UseVisualStyleBackColor = true;
+            // 
+            // btnSelect3
+            // 
+            this.btnSelect3.Location = new System.Drawing.Point(428, 231);
+            this.btnSelect3.Name = "btnSelect3";
+            this.btnSelect3.Size = new System.Drawing.Size(78, 36);
+            this.btnSelect3.TabIndex = 2;
+            this.btnSelect3.Text = "3";
+            this.btnSelect3.UseVisualStyleBackColor = true;
+            // 
+            // btnSelect4
+            // 
+            this.btnSelect4.Location = new System.Drawing.Point(615, 231);
+            this.btnSelect4.Name = "btnSelect4";
+            this.btnSelect4.Size = new System.Drawing.Size(78, 36);
+            this.btnSelect4.TabIndex = 3;
+            this.btnSelect4.Text = "4";
+            this.btnSelect4.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20F);
+            this.label1.Location = new System.Drawing.Point(83, 33);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 27);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "콜라";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20F);
+            this.label2.Location = new System.Drawing.Point(247, 33);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 27);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "사이다";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 20F);
+            this.label3.Location = new System.Drawing.Point(443, 33);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(39, 27);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "물";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("굴림", 20F);
+            this.label4.Location = new System.Drawing.Point(578, 33);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(147, 27);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "파워에이드";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("굴림", 20F);
+            this.label5.Location = new System.Drawing.Point(75, 192);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(87, 27);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "500원";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("굴림", 20F);
+            this.label6.Location = new System.Drawing.Point(253, 192);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(87, 27);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "500원";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("굴림", 20F);
+            this.label7.Location = new System.Drawing.Point(427, 192);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(87, 27);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "800원";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("굴림", 20F);
+            this.label8.Location = new System.Drawing.Point(599, 192);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(103, 27);
+            this.label8.TabIndex = 5;
+            this.label8.Text = "1000원";
+            // 
+            // lblCharge
+            // 
+            this.lblCharge.AutoSize = true;
+            this.lblCharge.Font = new System.Drawing.Font("굴림", 20F);
+            this.lblCharge.Location = new System.Drawing.Point(628, 347);
+            this.lblCharge.Name = "lblCharge";
+            this.lblCharge.Size = new System.Drawing.Size(54, 27);
+            this.lblCharge.TabIndex = 9;
+            this.lblCharge.Text = "___";
+            // 
+            // drinkBox
+            // 
+            this.drinkBox.Image = ((System.Drawing.Image)(resources.GetObject("drinkBox.Image")));
+            this.drinkBox.Location = new System.Drawing.Point(290, 518);
+            this.drinkBox.Name = "drinkBox";
+            this.drinkBox.Size = new System.Drawing.Size(192, 84);
+            this.drinkBox.TabIndex = 10;
+            this.drinkBox.TabStop = false;
+            this.drinkBox.Visible = false;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::_0403DrinkMachine.Properties.Resources.powerade;
+            this.pictureBox4.Location = new System.Drawing.Point(583, 63);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(134, 101);
+            this.pictureBox4.TabIndex = 4;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::_0403DrinkMachine.Properties.Resources.water;
+            this.pictureBox3.Location = new System.Drawing.Point(397, 63);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(134, 101);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::_0403DrinkMachine.Properties.Resources.cider;
+            this.pictureBox2.Location = new System.Drawing.Point(223, 63);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(134, 101);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::_0403DrinkMachine.Properties.Resources.coke;
+            this.pictureBox1.Location = new System.Drawing.Point(48, 63);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(134, 101);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblMoneyLeft1
+            // 
+            this.lblMoneyLeft1.AutoSize = true;
+            this.lblMoneyLeft1.Font = new System.Drawing.Font("굴림", 15F);
+            this.lblMoneyLeft1.Location = new System.Drawing.Point(72, 353);
+            this.lblMoneyLeft1.Name = "lblMoneyLeft1";
+            this.lblMoneyLeft1.Size = new System.Drawing.Size(70, 20);
+            this.lblMoneyLeft1.TabIndex = 11;
+            this.lblMoneyLeft1.Text = "잔액 : ";
+            // 
+            // lblMoneyLeft2
+            // 
+            this.lblMoneyLeft2.AutoSize = true;
+            this.lblMoneyLeft2.Font = new System.Drawing.Font("굴림", 15F);
+            this.lblMoneyLeft2.Location = new System.Drawing.Point(148, 353);
+            this.lblMoneyLeft2.Name = "lblMoneyLeft2";
+            this.lblMoneyLeft2.Size = new System.Drawing.Size(0, 20);
+            this.lblMoneyLeft2.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(763, 702);
+            this.Controls.Add(this.lblMoneyLeft2);
+            this.Controls.Add(this.lblMoneyLeft1);
+            this.Controls.Add(this.drinkBox);
+            this.Controls.Add(this.lblCharge);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btnSelect4);
+            this.Controls.Add(this.btnSelect3);
+            this.Controls.Add(this.btnSelect2);
+            this.Controls.Add(this.btnSelect1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.drinkBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect1;
+        private System.Windows.Forms.Button btnSelect2;
+        private System.Windows.Forms.Button btnSelect3;
+        private System.Windows.Forms.Button btnSelect4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblCharge;
+        private System.Windows.Forms.PictureBox drinkBox;
+        private System.Windows.Forms.Label lblMoneyLeft1;
+        private System.Windows.Forms.Label lblMoneyLeft2;
+    }
+}
+
---
+++ 0403DrinkMachine/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0403DrinkMachine
+{
+    public partial class Form1: Form
+    {
+            List<Drink> drinks = new List<Drink> {
+                new Drink(1, "콜라", 500),
+                new Drink(2, "사이다", 500),
+                new Drink(3, "물", 800),
+                new Drink(4, "파워에이드", 1000)
+                };
+
+        int moneyLeft = 2000;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSelect1_Click(object sender, EventArgs e)
+        {
+            moneyLeft -= drinks[0].price;
+            MessageBox.Show(moneyLeft.ToString());
+        }
+    }
+
+    public class Drink
+    {
+        public int number;
+        public string name;
+        public int price;
+
+        public Drink(int number, string name, int price)
+        {
+            this.number = number;
+            this.name = name;
+            this.price = price;
+        }
+    }
+}
