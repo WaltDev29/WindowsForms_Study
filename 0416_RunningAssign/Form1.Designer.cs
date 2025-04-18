210a4456fb0d31dbc9f617cb3cf55c8b8a3486f6
WaltDev29 <dwdr29@gmail.com>
2025-04-18 09:04:35 +0900

    계속 커밋

---
+++ 0416_RunningAssign/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿using System.Drawing;
+
+namespace _0416_PositionAssign
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnQuit = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnUp
+            // 
+            this.btnUp.Font = new System.Drawing.Font("굴림", 30F);
+            this.btnUp.Location = new System.Drawing.Point(96, 28);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(66, 52);
+            this.btnUp.TabIndex = 1;
+            this.btnUp.Text = "▴";
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.movePlayer);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Font = new System.Drawing.Font("굴림", 30F);
+            this.btnDown.Location = new System.Drawing.Point(96, 86);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(66, 52);
+            this.btnDown.TabIndex = 2;
+            this.btnDown.Text = "▾";
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.movePlayer);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Font = new System.Drawing.Font("굴림", 30F);
+            this.btnLeft.Location = new System.Drawing.Point(38, 49);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(52, 61);
+            this.btnLeft.TabIndex = 3;
+            this.btnLeft.Text = "◂";
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.movePlayer);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Font = new System.Drawing.Font("굴림", 30F);
+            this.btnRight.Location = new System.Drawing.Point(168, 49);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(52, 61);
+            this.btnRight.TabIndex = 4;
+            this.btnRight.Text = "▸";
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.movePlayer);
+            // 
+            // btnQuit
+            // 
+            this.btnQuit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnQuit.Font = new System.Drawing.Font("휴먼옛체", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnQuit.Location = new System.Drawing.Point(140, 213);
+            this.btnQuit.Name = "btnQuit";
+            this.btnQuit.Size = new System.Drawing.Size(102, 56);
+            this.btnQuit.TabIndex = 5;
+            this.btnQuit.Text = "Quit";
+            this.btnQuit.UseVisualStyleBackColor = false;
+            this.btnQuit.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.BackColor = System.Drawing.Color.Transparent;
+            this.lblTitle.Font = new System.Drawing.Font("휴먼옛체", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.lblTitle.Location = new System.Drawing.Point(68, 158);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(120, 42);
+            this.lblTitle.TabIndex = 6;
+            this.lblTitle.Text = "Game";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("휴먼옛체", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btnStart.Location = new System.Drawing.Point(18, 213);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(102, 56);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start!";
+            this.btnStart.UseVisualStyleBackColor = false;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(259, 295);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnQuit);
+            this.Controls.Add(this.btnRight);
+            this.Controls.Add(this.btnLeft);
+            this.Controls.Add(this.btnDown);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnUp);
+            this.Name = "Form1";
+            this.Text = "Form1";            
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnQuit;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Button btnStart;
+    }
+}
+
---
+++ 0416_RunningAssign/Form2.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0416_PositionAssign
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        // 3초 후 게임 시작
+        int readyCount = 3;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            readyCount--;
+            if (readyCount == 0)
+            {
+                timer1.Stop();
+                timer2.Enabled = true;
+                lblReady.Visible = false;
+            }
+            lblReady.Text = readyCount.ToString();
+        }
+
+        // 키보드로 플레이
+        private void Form2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape) this.Close();
+            if (e.KeyCode == Keys.Up) movePlayerUp();
+            if (e.KeyCode == Keys.Down) movePlayerDown();
+            if (e.KeyCode == Keys.Left) movePlayerLeft();
+            if (e.KeyCode == Keys.Right) movePlayerRight();
+
+        }
+
+        // 플레이 시간 기록
+        int milSecond = 0;
+        int second = 0;
+        int minute = 0;
+        private void timeCount()
+        {
+            milSecond++;
+            //lblTime.Text = "Time : " + milSecond.ToString();
+            if (milSecond >= 50)
+            {
+                milSecond = 0;
+                second++;
+            }
+            if (second >= 60)
+            {
+                second = 0;
+                minute++;
+            }
+            if (minute >= 100)
+            {
+                milSecond = 0;
+                second = 0;
+                minute = 0;
+            }
+            lblTime.Text = "Time : " + minute.ToString("D2") + " : " + second.ToString("D2");
+        }
+
+        // 타이머 메서드.     // 접촉 여부 체크, 점수 계산
+        int isHit = 0;
+        int score = 0;
+        Random rand = new Random();
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            timeCount();
+            // 좌표 변수 선언, 초기화
+            int pX = picPlayer.Location.X;
+            int pY = picPlayer.Location.Y;
+            int tX = picCoin.Location.X;
+            int tY = picCoin.Location.Y;
+
+            // Target이 화면 끝까지 이동한 경우
+            if (tX < 0)
+            {
+                // 점수가 0이면 Game Over
+                if (score <= 0)
+                {
+                    isHit = 1;
+                    timer2.Stop();
+                    gameOver();
+                    return;
+                }
+                // Target 좌표 초기화
+                isHit = 0;
+                tX = 700;
+                tY = rand.Next(1, 8) * 70;
+                picCoin.Visible = true;
+                picCoin.Location = new Point(tX, tY);
+            }
+
+            // Target 왼쪽으로 이동
+            if (score >= 10) tX -= score;
+            else tX -= 10;
+            picCoin.Location = new Point(tX, picCoin.Location.Y);
+            // 점수 증가 or 감소
+            hitOrMiss(pX, pY, tX, tY);
+        }
+
+        // 게임 오버 메시지박스 출력
+        private void gameOver()
+        {
+            DialogResult ret = MessageBox.Show("Game Over", "Game Over", MessageBoxButtons.RetryCancel);
+            if (ret == DialogResult.Retry)
+            {
+                isHit = 0;
+                readyCount = 3;
+                lblReady.Text = "3";
+                lblReady.Visible = true;
+                timer1.Enabled = true;
+                picCoin.Location = new Point(631, 210);
+                lblTime.Text = "Time : 00 : 00";
+            }
+            else this.Close();
+        }
+
+        bool blink = false;
+        int blinkTime = 0;
+        // Target과 접촉 or Miss여부 판단
+        private void hitOrMiss(int pX, int pY, int tX, int tY)
+        {
+            // Target과 Player가 접촉한 경우
+            if ((tX <= pX + 50 && tX >= pX - 50) && (tY <= pY + 40 && tY >= pY - 40))
+            {
+                // 첫 접촉일 경우 (접촉 시 다중 작동하지 않도록)
+                if (isHit == 0)
+                {
+                    picCoin.Visible = false;
+                    isHit = 1;
+                    score++;
+                    blink = true;
+                    lblScore.Text = "Score : " + score.ToString();
+                    picBlink.Location = new Point(picCoin.Location.X, picCoin.Location.Y);
+                    picBlink.Visible = true;
+
+                }
+            }
+            if (blink == true)
+            {
+                blinkTime++;
+                if (blinkTime >= 40)
+                {
+                    blink = false;
+                    blinkTime = 0;
+                    picBlink.Visible = false;
+                }
+            }
+            // Target을 지나친경우, score가 0보다 높을 경우 (점수 마이너스 방지)
+            if ((tX < pX - 50 && isHit == 0) && score > 0)
+            {
+                isHit = 1;
+                score--;
+                lblScore.Text = "Score : " + score.ToString();
+            }
+        }
+
+        // 플레이어 좌표 이동
+        public void movePlayerUp()
+        {
+            int y = picPlayer.Location.Y;
+            if (y <= 60) return;
+            y -= 70;
+            picPlayer.Location = new Point(picPlayer.Location.X, y);
+        }
+        public void movePlayerDown()
+        {
+            int y = picPlayer.Location.Y;
+            if (y >= 420) return;
+            y += 70;
+            picPlayer.Location = new Point(picPlayer.Location.X, y);
+        }
+        public void movePlayerLeft()
+        {
+            int x = picPlayer.Location.X;
+            if (x <= 30) return;
+            x -= 70;
+            picPlayer.Location = new Point(x, picPlayer.Location.Y);
+        }
+        public void movePlayerRight()
+        {
+            int x = picPlayer.Location.X;
+            if (x >= 510) return;
+            x += 70;
+            picPlayer.Location = new Point(x, picPlayer.Location.Y);
+        }
+    }
+}
