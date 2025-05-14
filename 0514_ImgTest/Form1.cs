1b9205b576e38bfed05f66fc4b7850823e7b14cb
WaltDev29 <dwdr29@gmail.com>
2025-05-14 14:30:26 +0900

    img 사용방식 테스트

---
+++ 0514_ImgTest/Form1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _0514_ImgTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();            
+            pictureBox2.ImageLocation = "Images/guy_right.png";
+        }
+    }
+}
