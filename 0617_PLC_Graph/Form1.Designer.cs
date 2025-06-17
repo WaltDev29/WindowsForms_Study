a1fc886cab66d72271f27f9137c50aebfbbcbf30
WaltDev29 <waltdev29@gmail.com>
2025-06-17 22:01:19 +0900

    practice

---
+++ 0617_PLC_Graph/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace _0617_PLC_Graph
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.btn_connect = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_connect
+            // 
+            this.btn_connect.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.btn_connect.Location = new System.Drawing.Point(356, 26);
+            this.btn_connect.Name = "btn_connect";
+            this.btn_connect.Size = new System.Drawing.Size(72, 34);
+            this.btn_connect.TabIndex = 0;
+            this.btn_connect.Text = "연결";
+            this.btn_connect.UseVisualStyleBackColor = true;
+            this.btn_connect.Click += new System.EventHandler(this.btn_connect_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(560, 136);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 23;
+            this.dataGridView1.Size = new System.Drawing.Size(240, 315);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(-2, 136);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.Legend = "Legend1";
+            series5.Name = "Series1";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(564, 315);
+            this.chart1.TabIndex = 2;
+            this.chart1.Text = "chart1";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btn_connect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_connect;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ 0617_PLC_Graph/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using ACTMULTILIB_K;
+
+namespace _0617_PLC_Graph
+{
+    public partial class Form1 : Form
+    {
+        ActEasyIF plc;
+        public Form1()
+        {
+            InitializeComponent();
+            plc = new ActEasyIF();
+        }
+
+        short value;
+        short sens;
+        bool cylB, cylC, liftA, liftB, liftA_sens, liftB_sens;
+
+        private void btn_connect_Click(object sender, EventArgs e)
+        {
+            if (plc.Open() == 0)
+            {
+                MessageBox.Show("연결되었습니다.");
+                timer1.Enabled = true;
+            }
+            else MessageBox.Show("연결되었습니다.");
+        }
+
+        // 실린더 제어 함수
+        private void mv_cyl(char target, char direction)
+        {
+            ushort reset = 0;
+            ushort mask = 0;
+            plc.ReadDeviceBlock2("Y0", 1, out sens);
+            if (target == 'B')
+            {
+                reset = 0b1111111111111001;
+                if (direction == 'F') mask = 0b0000000000000010;
+                else if (direction == 'B') mask = 0b0000000000000100;
+            }
+            else if (target == 'C')
+            {
+                reset = 0b1111111111100111;
+                if (direction == 'F') mask = 0b0000000000001000;
+                else if (direction == 'B') mask = 0b0000000000010000;
+            }
+            value = (short)((sens & reset) | mask);
+            plc.WriteDeviceBlock2("Y0", 1, ref value);
+        }
+        // 리프트 제어 함수
+        private void mv_lift(char target, char direction)
+        {
+            ushort reset = 0;
+            ushort mask = 0;
+            plc.ReadDeviceBlock2("Y0", 1, out sens);
+            if (target == 'A')
+            {
+                reset = 0b1111111110011111;
+                if (direction == 'U') mask = 0b0000000000100000;
+                else if (direction == 'D') mask = 0b0000000001000000;
+            }
+            else if (target == 'B')
+            {
+                reset = 0b1111111001111111;
+                if (direction == 'U') mask = 0b0000000100000000;
+                else if (direction == 'D') mask = 0b0000000010000000;
+            }
+            value = (short)((sens & reset) | mask);
+            plc.WriteDeviceBlock2("Y0", 1, ref value);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            plc.ReadDeviceBlock2("X0", 1, out sens);
+
+            // cyl B
+            if ((sens & 0x04) != 0) cylB = true;
+            else if ((sens & 0x08) != 0) cylB = false;
+            // cyl C
+            if ((sens & 0x20) != 0) cylC = true;
+            else if ((sens & 0x10) != 0) cylC = false;
+            // lift A
+            if ((sens & 0x40) != 0) liftA = true;
+            else if ((sens & 0x80) != 0) liftA = false;
+            // lift B
+            if ((sens & 0x100) != 0) liftB = true;
+            else if ((sens & 0x200) != 0) liftB = false;
+            // lift A sens
+            if ((sens & 0x400) != 0) liftA_sens = true;
+            else liftA_sens = false;
+            // lift B sens
+            if ((sens & 0x800) != 0) liftB_sens = true;
+            else liftB_sens = false;
+
+            autoLoop();
+        }
+
+        private void autoLoop()
+        {
+            if (liftA_sens && liftA) mv_lift('B', 'U');
+            if (liftA_sens && liftB) mv_cyl('B', 'F');
+            if (liftB_sens && liftB)
+            {
+                mv_lift('A', 'D');
+                mv_lift('B', 'D');
+            }
+            if (liftB_sens && !liftB) mv_cyl('C', 'F');
+            if (liftA_sens && !liftA)
+            {
+                mv_lift('A', 'U');
+                mv_lift('B', 'U');
+            }
+
+            if (cylB) mv_cyl('B', 'B');
+            if (cylC) mv_cyl('C', 'B');
+        }
+    }
+}
